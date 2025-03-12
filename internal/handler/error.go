3325a3f3bcbfa2362df23a8fc30447d41f7716f3
dongwlin <dongwlin@foxmail.com>
2025-03-12 23:57:28 +0800

    fix: add fiber default error handler

--- internal/handler/error.go
+++ internal/handler/error.go
@@ -4,6 +4,7 @@ import (
 	"github.com/MirrorChyan/resource-backend/internal/handler/response"
 	"github.com/MirrorChyan/resource-backend/internal/pkg/errs"
 	"github.com/gofiber/fiber/v2"
+	"go.uber.org/zap"
 )
 
 func Error(c *fiber.Ctx, err error) error {
@@ -15,11 +16,22 @@ func Error(c *fiber.Ctx, err error) error {
 	)
 
 	switch e := err.(type) {
+
+	case *fiber.Error:
+
+		return fiber.DefaultErrorHandler(c, e)
+
 	case *errs.Error:
+
 		statusCode = e.StatusCode
 		msg = e.Message
 		data = e.Data
+
 	default:
+
+		zap.L().Error("Unexpected error",
+			zap.Error(err),
+		)
 		resp := response.UnexpectedError()
 		return c.Status(fiber.StatusInternalServerError).JSON(resp)
 	}
